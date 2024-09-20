06a482a95539a940dba9bf267e7ddf527c4ac833
Jim Orcheson <jimorcheson@gmail.com>
2024-09-20 12:06:53 -0400

    Add debug and log filename command line options

--- cmd/jsdr/main.go
+++ cmd/jsdr/main.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/jimorc/jsdr/internal/logger"
+
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	fmt.Println("Hello from jsdr")
+	logLevel, logFile := parseCommandLine()
+	fmt.Printf("logLevel = %d\n", logLevel)
+	fmt.Printf("logFile = %s\n", logFile)
+}
+
+func parseCommandLine() (logger.LoggingLevel, string) {
+	pflag.Bool("debug", false, "Log debug information")
+	pflag.String("out", os.Getenv("HOME")+"/jsdr.log", "Log filename")
+	pflag.Parse()
+	viper.BindPFlags(pflag.CommandLine)
+	debug := viper.GetBool("debug")
+	logFile := viper.GetString("out")
+	logLevel := logger.Info
+	if debug {
+		logLevel = logger.Debug
+	}
+	return logLevel, logFile
 }
