bebdc0411562501d922237c9720b9f9d5e6d8bb8
Jim Orcheson <jimorcheson@gmail.com>
2024-09-22 10:21:12 -0400

    Create and display Settings dialog
    display Settings dialog when settings action clicked.
    Just log dialog callback value.

--- internal/ui/mainWindow.go
+++ internal/ui/mainWindow.go
@@ -8,16 +8,17 @@ import (
 )
 
 var uiLogger *logger.Logger
+var mainWin fyne.Window
 
 func MakeMainWindow(jsdrApp *fyne.App, log *logger.Logger) fyne.Window {
 	uiLogger = log
-	win := (*jsdrApp).NewWindow("jsdr")
+	mainWin = (*jsdrApp).NewWindow("jsdr")
 
 	uiLogger.Log(logger.Debug, "Creating main window content\n")
 	settingsAction := makeSettingsAction()
 	toolbar := widget.NewToolbar(settingsAction)
-	win.SetContent(container.NewBorder(toolbar, nil, nil, nil))
-	win.Resize(fyne.NewSize(400, 200))
+	mainWin.SetContent(container.NewBorder(toolbar, nil, nil, nil))
+	mainWin.Resize(fyne.NewSize(400, 200))
 	uiLogger.Log(logger.Debug, "Main window content created\n")
-	return win
+	return mainWin
 }
--- internal/ui/settingsaction.go
+++ internal/ui/settingsaction.go
@@ -1,6 +1,8 @@
 package ui
 
 import (
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 
@@ -9,9 +11,19 @@ import (
 
 func makeSettingsAction() *widget.ToolbarAction {
 	uiLogger.Log(logger.Debug, "Entered ui.makeSettingsAction\n")
-	action := widget.NewToolbarAction(theme.SettingsIcon(), func() {
-		uiLogger.Log(logger.Debug, "Inside settingsAction callback\n")
-	})
+	action := widget.NewToolbarAction(theme.SettingsIcon(), settingsCallback)
 	uiLogger.Log(logger.Debug, "Returning the settings toolbar action from makeSettingsAction\n")
 	return action
+}
+
+func settingsCallback() {
+	uiLogger.Log(logger.Debug, "In settingsCallback\n")
+	label := widget.NewLabel("Settings")
+	content := container.NewVBox(label)
+	settings := dialog.NewCustomConfirm("SDR Settings", "Accept", "Close", content, settingsDialogCallback, mainWin)
+	settings.Show()
+}
+
+func settingsDialogCallback(accept bool) {
+	uiLogger.Logf(logger.Debug, "In settingsDialogCallback: %v\n", accept)
 }
