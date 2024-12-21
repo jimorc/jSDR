ef6447f41486e4226877ba56826405cef93df2a3
Jim Orcheson <jimorcheson@gmail.com>
2024-12-21 11:36:09 -0500

    Fix infinite loops
    Missing pointers to actual device.

--- internal/sdr/soapydevice_stream.go
+++ internal/sdr/soapydevice_stream.go
@@ -6,10 +6,10 @@ import (
 
 // GetStreamFormats retrieves the formats for the device's specified direction and channel.
 func (sD *SoapyDevice) GetStreamFormats(direction device.Direction, channel uint) []string {
-	return sD.GetStreamFormats(direction, channel)
+	return sD.Device.Device.GetStreamFormats(direction, channel)
 }
 
 // GetNativeStreamFormat retrieves the native format and full scale value for the specified channel and direction.
 func (sD *SoapyDevice) GetNativeStreamFormat(direction device.Direction, channel uint) (string, float64) {
-	return sD.GetNativeStreamFormat(direction, channel)
+	return sD.Device.Device.GetNativeStreamFormat(direction, channel)
 }
